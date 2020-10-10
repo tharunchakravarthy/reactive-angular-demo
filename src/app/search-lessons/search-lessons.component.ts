ffebb37af3ea4a369595748886a0c616bb98cb76
tharunchakravarthy <tharunvanam456@gmail.com>
2020-10-11 03:30:44 +0530

    search-results-setup

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from 'app/services/courses.service';
 
 
 @Component({
@@ -24,7 +25,9 @@ import {Lesson} from '../model/lesson';
 })
 export class SearchLessonsComponent implements OnInit {
 
-  constructor() {
+  searchResults$ : Observable<Lesson[]>;
+
+  constructor(private courseService: CoursesService) {
 
 
   }
@@ -32,6 +35,10 @@ export class SearchLessonsComponent implements OnInit {
   ngOnInit() {
 
 
+  }
+
+  onSearch(search: string) {
+    this.searchResults$ = this.courseService.searchCourses(search);
   }
 
 }
